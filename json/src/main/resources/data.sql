14df82ab5d2e65e56599e249e494199ccce1a5d5
Egor Pozdnov <pozdner98@gmail.com>
2025-09-08 01:40:10 +0300

    add dump

--- json/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-insert into manager (account_non_expired, account_non_locked, credentials_non_expired, enabled, password, username)
-VALUES (true, true, true, true, '{bcrypt}$2a$10$mRbur.wbfxmFkq3X/vMO/eZfrOswp1MGI2fzRx63FNdwkXvBn.0ha', 'manager1');
-
-
-insert into enterprise (id, city, name, production_capacity, zone)
-VALUES ('362fb5e1-a4f3-4e7b-b757-f1e6e592144c', 'CIIITY', 'NAME', 123, 'UTC+04:00');
-
-insert into manager_enterprise (enterprise_id, manager_id)
-VALUES ('362fb5e1-a4f3-4e7b-b757-f1e6e592144c', 1);
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,13 +5,27 @@ echo "🚀 Starting deployment..."
 
 # Добавляем шаг сборки проекта с помощью Maven
 echo "📦 Building the project with Maven..."
+
+echo "Начало в:"
+date
+
 mvn clean package
+
+echo "Конец в:"
+date
 
 echo "🐳 Navigating to the docker-compose file..."
 # Скрипт уже находится в папке diplom, поэтому путь относительный
 cd ./json
 
 echo "🚀 Rebuilding and deploying containers..."
+
+echo "Начало в:"
+date
+
 docker-compose up --build -d --remove-orphans
+
+echo "Конец в:"
+date
 
 echo "✅ Deployment finished successfully!"
